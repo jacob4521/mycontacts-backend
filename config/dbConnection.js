c218ff3fe6bea273f8626e19d67db759b7ba51f1
Zakib <zakib6354@gmail.com>
2026-01-08 18:15:21 +0530

    Fix typo in environment variable name for database connection

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
-    const connect = await mongoose.connect(process.env.CONECTION_STRING);
+    const connect = await mongoose.connect(process.env.CONNECTION_STRING);
     console.log(
       'Database Connected: ',
       connect.connection.host,
