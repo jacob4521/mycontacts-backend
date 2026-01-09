d5e29254042300bcb6d0d6ece0020fb5d53bd44c
Zakib <zakib6354@gmail.com>
2026-01-09 11:30:45 +0530

    Implement user registration, login, and current user routes with controller functions

---
+++ models/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, 'Please add a username'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please add an email'],
+    unique: [true, 'Email already exists'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: [6, 'Password must be at least 6 characters'],
+  }
+},
+{
+  timestamps: true,
+});
+
+module.exports = mongoose.model('User', userSchema);
